e5560aa6e0461e3e96f47769f39f4fc6f0f3fed4
bikramgiri <asgbrg.github@gmail.com>
2026-03-26 17:23:02 +0545

    add server files properly

---
+++ Server/app.js
@@ -0,0 +1,12 @@
+require ("dotenv").config ();
+const express = require ("express");
+const app = express ();
+
+app.get ("/", (req, res) => {
+    res.send ("Hello World!");
+});
+
+const PORT = process.env.PORT;
+app.listen (PORT, () => {
+    console.log (`Server is running on port ${PORT}`);
+});
